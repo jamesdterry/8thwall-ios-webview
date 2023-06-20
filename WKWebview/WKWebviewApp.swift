e5f2d855859b371d6500f5d18c230ba3227886ed
Evan Carlson <evancarlson@8thwall.com>
2023-06-20 12:43:57 -0700

    Initial Commit

---
+++ WKWebview/WKWebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKWebviewApp.swift
+//  WKWebview
+//
+//  Created by Evan Carlson on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WKWebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
